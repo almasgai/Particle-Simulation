7b765d76f922452aac42506108e1424bdb5d461c
Al Masgai <alex.masgai@gmail.com>
2018-01-05 13:51:13 -0500

    Initial Commit

---
+++ Particles/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Particles
+//
+//  Created by Al Masgai on 1/5/18.
+//  Copyright © 2018 Al Masgai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
