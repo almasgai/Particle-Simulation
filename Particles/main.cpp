441a54b4952c4ce045851a9bf36b4f9127ceea11
Al Masgai <alex.masgai@gmail.com>
2018-01-05 23:14:30 -0500

    Added basic black screen

--- Particles/main.cpp
+++ Particles/main.cpp
@@ -29,11 +29,42 @@ int main( int argc, char* args[] )
     
     SDL_Window * window = SDL_CreateWindow("Simulation", SDL_WINDOWPOS_UNDEFINED,
                                            SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
-    
+   
     if (window == NULL) {
         SDL_Quit();
-        return 1;
+        return 2;
     }
+    // Creating a renderer
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    
+    // Creating a texture
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_BGRA8888, SDL_TEXTUREACCESS_STATIC, WIDTH, HEIGHT);
+    
+    // If renderer or texture equals NULL, abort program
+    if (renderer == NULL) {
+        cout << "Could not create renderer" << endl;
+        
+        // Closes window
+        SDL_DestroyWindow(window);
+        
+        // Called after destroying window to clean up any resources SDL is using
+        SDL_Quit();
+        return 2;
+    }
+    if (texture == NULL) {
+        cout << "Could not create texture" << endl;
+        
+        // Closes window
+        SDL_DestroyWindow(window);
+        
+        // Called after destroying window to clean up any resources SDL is using
+        SDL_Quit();
+        return 2;
+    }
+    
+    // Creating an unsigned 32 bit int because not all systems may have 32 bit ints
+    // SDL allow ints to be explicitly declared as 32 bits
+    Uint32 * buffer = new Uint32[WIDTH * HEIGHT];
     
     // Creating a loop so that the window can be constantly updated
     while (!quitProgram) {
@@ -46,6 +77,14 @@ int main( int argc, char* args[] )
         
     }
     
+    // Deleting Buffer
+    delete buffer;
+    
+    // Destroy renderer
+    SDL_DestroyRenderer(renderer);
+    
+    // Destroy Texture
+    SDL_DestroyTexture(texture);
     
     // Close window
     SDL_DestroyWindow(window);
