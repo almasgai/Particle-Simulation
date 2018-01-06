4de34e7aea8682555e8c1e3f603afa641b87eb37
Al Masgai <alex.masgai@gmail.com>
2018-01-05 19:14:11 -0500

    Initial commit

--- Particles/main.cpp
+++ Particles/main.cpp
@@ -6,10 +6,58 @@
 //  Copyright © 2018 Al Masgai. All rights reserved.
 //
 
+#include <SDL.h>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define WIDTH   800
+#define HEIGHT  600
+
+using namespace std;
+
+int main( int argc, char* args[] )
+{
+    // Tells the main loop of the program when to quit & exit
+    bool quitProgram = false;
+    
+    // Creating an SDL Event object for the loop
+    SDL_Event event;
+    
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        cout << "SDL FAILED" << endl;
+        return 1;
+    }
+    
+    SDL_Window * window = SDL_CreateWindow("Simulation", SDL_WINDOWPOS_UNDEFINED,
+                                           SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    
+    if (window == NULL) {
+        SDL_Quit();
+        return 1;
+    }
+    
+    // Creating a loop so that the window can be constantly updated
+    while (!quitProgram) {
+        
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quitProgram = true;
+            }
+        }
+        
+    }
+    
+    
+    // Close window
+    SDL_DestroyWindow(window);
+    // Cleans up any resource(s) SDL is using
+    SDL_Quit();
+    
     return 0;
 }
+
+
+
+
+
+
+
